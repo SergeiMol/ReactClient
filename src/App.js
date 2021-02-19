10dde746c2007cc355ecacd53014c9111799a4c0
SergeiMol <sergei.molohvey@gmail.com>
2021-02-20 00:32:07 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {Switch, Route, Link} from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -33,79 +34,77 @@ const App = () => {
         return <Redirect to="/login"/>;
     };
 
-    render()
-    {
-        return (
-            <>
-                <nav className="navbar navbar-expand navbar-dark bg-dark">
-                    <div className="navbar-nav mr-auto">
-                        {currentUser && (
-                            <li className="nav-item">
-                                <Link to={"/home"} className="nav-link">
-                                    Home
-                                </Link>
-                            </li>
-                        )}
 
-                        {showModeratorBoard && (
-                            <li className="nav-item">
-                                <Link to={"/mod"} className="nav-link">
-                                    Moderator Board
-                                </Link>
-                            </li>
-                        )}
-
-                        {showAdminBoard && (
-                            <li className="nav-item">
-                                <Link to={"/admin"} className="nav-link">
-                                    Admin Board
-                                </Link>
-                            </li>
-                        )}
-                    </div>
-
-                    {currentUser ? (
-                        <div className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <Link to={"/profile"} className="nav-link">
-                                    {currentUser.username}
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <a href="" className="nav-link" onClick={logOut}>
-                                    LogOut
-                                </a>
-                            </li>
-                        </div>
-                    ) : (
-                        <div className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <Link to={"/login"} className="nav-link">
-                                    Login
-                                </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to={"/register"} className="nav-link">
-                                    Sign Up
-                                </Link>
-                            </li>
-                        </div>
+    return (
+        <>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="navbar-nav mr-auto">
+                    {currentUser && (
+                        <li className="nav-item">
+                            <Link to={"/home"} className="nav-link">
+                                Home
+                            </Link>
+                        </li>
                     )}
-                </nav>
 
-                <div className="container mt-3">
-                    <Switch>
-                        <Route exact path="/home" component={Home}/>
-                        <Route exact path="/login" component={Login}/>
-                        <Route exact path="/register" component={Register}/>
-                        <Route exact path="/profile" component={Profile}/>
-                        <Route path="/user" component={BoardUser}/>
-                    </Switch>
+                    {showModeratorBoard && (
+                        <li className="nav-item">
+                            <Link to={"/mod"} className="nav-link">
+                                Moderator Board
+                            </Link>
+                        </li>
+                    )}
+
+                    {showAdminBoard && (
+                        <li className="nav-item">
+                            <Link to={"/admin"} className="nav-link">
+                                Admin Board
+                            </Link>
+                        </li>
+                    )}
                 </div>
-            </>
-        );
-    }
+
+                {currentUser ? (
+                    <div className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link to={"/profile"} className="nav-link">
+                                {currentUser.username}
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a href="" className="nav-link" onClick={logOut}>
+                                LogOut
+                            </a>
+                        </li>
+                    </div>
+                ) : (
+                    <div className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link to={"/login"} className="nav-link">
+                                Login
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link to={"/register"} className="nav-link">
+                                Sign Up
+                            </Link>
+                        </li>
+                    </div>
+                )}
+            </nav>
+
+            <div className="container mt-3">
+                <Switch>
+                    <Route exact path="/home" component={Home}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/register" component={Register}/>
+                    <Route exact path="/profile" component={Profile}/>
+                    <Route path="/user" component={BoardUser}/>
+                </Switch>
+            </div>
+        </>
+    );
 };
 
 export default App;
