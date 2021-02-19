fa67867f5f1ca41facc36fa8fcc5f75a033f2c74
SergeiMol <sergei.molohvey@gmail.com>
2021-02-19 23:09:49 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
                 <div className="navbar-nav mr-auto">
                     {currentUser && (
                         <li className="nav-item">
-                            <Link to={"home"} className="nav-link">
+                            <Link to={"/home"} className="nav-link">
                                 Home
                             </Link>
                         </li>
@@ -45,7 +45,7 @@ const App = () => {
 
                     {showModeratorBoard && (
                         <li className="nav-item">
-                            <Link to={"mod"} className="nav-link">
+                            <Link to={"/mod"} className="nav-link">
                                 Moderator Board
                             </Link>
                         </li>
@@ -53,7 +53,7 @@ const App = () => {
 
                     {showAdminBoard && (
                         <li className="nav-item">
-                            <Link to={"admin"} className="nav-link">
+                            <Link to={"/admin"} className="nav-link">
                                 Admin Board
                             </Link>
                         </li>
@@ -63,7 +63,7 @@ const App = () => {
                 {currentUser ? (
                     <div className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <Link to={"profile"} className="nav-link">
+                            <Link to={"/profile"} className="nav-link">
                                 {currentUser.username}
                             </Link>
                         </li>
@@ -76,13 +76,13 @@ const App = () => {
                 ) : (
                     <div className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <Link to={"login"} className="nav-link">
+                            <Link to={"/login"} className="nav-link">
                                 Login
                             </Link>
                         </li>
 
                         <li className="nav-item">
-                            <Link to={"register"} className="nav-link">
+                            <Link to={"/register"} className="nav-link">
                                 Sign Up
                             </Link>
                         </li>
@@ -92,11 +92,11 @@ const App = () => {
 
             <div className="container mt-3">
                 <Switch>
-                    <Route exact path="home" component={Home}/>
-                    <Route exact path="login" component={Login}/>
-                    <Route exact path="register" component={Register}/>
-                    <Route exact path="profile" component={Profile}/>
-                    <Route path="user" component={BoardUser}/>
+                    <Route exact path="/home" component={Home}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/register" component={Register}/>
+                    <Route exact path="/profile" component={Profile}/>
+                    <Route path="/user" component={BoardUser}/>
                 </Switch>
             </div>
         </>
