37652fdde706c1aa43afd9aa413944217a743095
SergeiMol <sergei.molohvey@gmail.com>
2021-02-20 00:14:32 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Register from "./components/Register";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import BoardUser from "./components/BoardUser";
+import Redirect from "react-router/modules/Redirect";
+
 
 
 const App = () => {
@@ -28,7 +30,8 @@ const App = () => {
     }, []);
 
     const logOut = () => {
-        AuthService.logout();
+        AuthService.logout()
+        return <Redirect to="/users" />;;
     };
 
     return (
