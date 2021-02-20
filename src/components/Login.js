a14f36ac284f4ba1dc75d53105096a9c12f24b91
SergeiMol <sergei.molohvey@gmail.com>
2021-02-20 21:50:23 +0300

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -46,7 +46,6 @@ const Login = (props) => {
             AuthService.login(username, password).then(
                 () => {
                     props.history.push("/home");
-                    window.location.reload();
                 },
                 (error) => {
                     const resMessage =
