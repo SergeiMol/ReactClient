2057dae8ea16f096c5c801b569d7d16873229c5a
SergeiMol <sergei.molohvey@gmail.com>
2021-02-20 22:43:23 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ const App = () => {
 
             <div className="container mt-3">
                 <Switch>
-                    <Route exact path="/home" component={Home}/>
+                    <Route path="/home" component={Home}/>
                     <Route exact path="/login" component={Login}/>
                     <Route exact path="/register" component={Register}/>
                     <Route exact path="/profile" component={Profile}/>
